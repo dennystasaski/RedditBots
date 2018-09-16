bd5328ab37eadfe6c7d028e25bc0123a242ee23e
dennystasaski <denny.stasaski@utexas.edu>
2018-09-16 17:27:33 -0500

    Test comments working

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RedditBots
+Set up config.py with properties from reddit application page
--- SimpleBot.py
+++ SimpleBot.py
@@ -3,6 +3,22 @@ import config
 
 def bot_login():
 	print("Attempting to log in")
+	r = praw.Reddit(username = config.username,
+			password = config.password,
+			client_id = config.client_id,
+			client_secret = config.client_secret,
+			user_agent = "A very cool bot v0.1")
+	print("Successfully logged in")
+	return r
 
+def reply_to_comment(r):
+	for comment in r.subreddit('test').comments(limit=25):
+		if "sir" in comment.body:
+			comment.reply("I am testing my bot.  Congrats!")
 
-bot_login()
+def submit_text_post(r):
+	r.submit('test', 'My cool bot\'s first post!', text='Test body')
+r = bot_login()
+# loop here for constant running
+# reply_to_comment(r)
+submit_text_post(r)
